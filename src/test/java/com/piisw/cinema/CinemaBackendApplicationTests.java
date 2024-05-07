180fa51ee7ec445aca6f708f31a7ce59ce6c8e0d
Mateusz Szlachetko <mateusz.szlachetko@gmail.com>
2024-05-07 19:25:43 +0200

    chore: project initialization

---
+++ src/test/java/com/piisw/cinema/CinemaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piisw.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
